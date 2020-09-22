320660d42b5410c19779ddd4a1f2519c3f79e2fd
Yves Riedener <git@reecube.com>
2020-09-22 16:31:10 +0200

    WIP

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
-import DarkHeaderA from './lib/header/dark/a';
-import DarkBlogA from './lib/blog/dark/a';
-import DarkFooterA from './lib/footer/dark/a';
+import HeaderA from './lib/header/a';
+
+const onToggleClick = () => {
+  const attr = 'data-mode';
+  const rootNode = document.documentElement;
+
+  const isDark = rootNode.getAttribute(attr) === 'dark';
+  rootNode.setAttribute(attr, isDark ? 'light' : 'dark');
+};
+
 
 const App = () => (
-  <main className="mode-dark">
-    <div className="bg-blue-100 text-blue-900 dark:bg-blue-900 dark:text-blue-100">
-      Hello World
-    </div>
-    <DarkHeaderA />
-    <DarkBlogA />
-    <DarkFooterA />
+  <main>
+    <button
+      className="bg-blue-100 dark:bg-blue-900 hover:bg-blue-300 dark-hover:bg-blue-600 text-blue-900 dark:text-blue-100 font-bold py-2 px-4 rounded uppercase m-auto block space-y-16"
+      style={{ margin: '100px auto' }}
+      onClick={onToggleClick}>
+      Toggle Dark Mode
+    </button>
+
+    <HeaderA />
   </main>
 );
 
